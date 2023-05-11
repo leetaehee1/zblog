e035156b9385395283b95f34f740e9572502366e
310-1 <310-1@310-21>
2023-05-11 14:48:41 +0900

    첫화면 완성

---
+++ src/main/java/com/sg/leo/domain/User.java
@@ -0,0 +1,48 @@
+package com.sg.leo.domain;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="USERS")
+
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable=false, length=50, unique=true)
+	private String username;
+	
+	@Column(length=100)
+	private String password;
+	
+	@Column(nullable=false, length=120)
+	private String email;
+	
+	@Enumerated(EnumType.STRING)
+	private RoleType role;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
+   
